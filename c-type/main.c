c29eb4033c2eb8b84742f667f3175f6f54ca1f1a
adrag <adragota98@gmail.com>
2024-01-31 14:40:26 -0800

    Initial test commit of C project.

---
+++ c-type/main.c
@@ -0,0 +1,17 @@
+
+#include<stdio.h>
+
+int main()
+{	
+	int user_input;
+
+
+	printf("Enter an integer\n");
+
+	scanf("%d", &user_input);
+
+	printf("You entered: %d\n", user_input);
+
+	return 0;
+}
+
